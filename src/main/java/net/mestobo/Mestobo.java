ff00dfeaf0fca041a46df705bc527e4babf34a03
Patrick Peer <ppe@synedra.com>
2022-05-31 15:31:49 +0200

    Separated main class from Application class

--- src/main/java/net/mestobo/Mestobo.java
+++ src/main/java/net/mestobo/Mestobo.java
@@ -3,36 +3,12 @@
  */
 package net.mestobo;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Mestobo extends Application {
+/**
+ * Thin wrapper to separate the main class from the JavaFX Application class.
+ * This simplifies launching from IDEs
+ */
+public class Mestobo {
 	public static void main(String[] args) {
-		launch(args);
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Hello World!");
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
-
+		MestoboApplication.start(args);
 	}
 }
---
+++ src/main/java/net/mestobo/MestoboApplication.java
@@ -0,0 +1,35 @@
+package net.mestobo;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MestoboApplication extends Application {
+    public static void start(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+
+    }
+}
