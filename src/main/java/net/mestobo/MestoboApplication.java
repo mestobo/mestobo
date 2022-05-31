90f6ceb327b44d482db627738e5e476bd3771d45
Patrick Peer <ppe@synedra.com>
2022-05-31 15:48:52 +0200

    Removed obsolete import

--- src/main/java/net/mestobo/MestoboApplication.java
+++ src/main/java/net/mestobo/MestoboApplication.java
@@ -2,7 +2,6 @@ package net.mestobo;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
