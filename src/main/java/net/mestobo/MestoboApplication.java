2e0ad9b6936a0ebe99e6ee139ca8f31501b8a0c8
Robert Lichtenberger <r.lichtenberger@synedra.com>
2022-10-07 11:46:58 +0200

    Test JMetro.

--- src/main/java/net/mestobo/MestoboApplication.java
+++ src/main/java/net/mestobo/MestoboApplication.java
@@ -3,6 +3,7 @@ package net.mestobo;
 import com.panemu.tiwulfx.control.dock.DetachableTabPane;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane.TabClosingPolicy;
@@ -10,6 +11,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.JMetroStyleClass;
+import jfxtras.styles.jmetro.Style;
 
 public class MestoboApplication extends Application {
 
@@ -33,9 +37,10 @@ public class MestoboApplication extends Application {
 		primaryStage.setTitle(I18N.get("WindowTitle"));
 
         StackPane root = new StackPane();
-        VBox vbox = new VBox();
-        
-        createTabPane();
+        VBox vbox = GUIFactory.create(VBox.class, root, "mainbox");
+        vbox.getStyleClass().add(JMetroStyleClass.BACKGROUND);					
+                
+        createTabPane(vbox);
         vbox.getChildren().addAll(new MestoboMenuBar(), tabPane);
         
         root.getChildren().add(vbox);
@@ -44,13 +49,17 @@ public class MestoboApplication extends Application {
 
 		scene = new Scene(root, primaryScreen.getBounds().getWidth() / 2, primaryScreen.getBounds().getHeight() / 2);
 		scene.getStylesheets().add("mestobo.css");
+		JMetro jMetro = new JMetro(Style.LIGHT);
+		jMetro.setScene(scene);
+		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	private void createTabPane() {
-		tabPane = new DetachableTabPane();
+	private void createTabPane(Node parent) {
+		tabPane = GUIFactory.create(DetachableTabPane.class, parent, "tabpane");
 		tabPane.setTabClosingPolicy(TabClosingPolicy.ALL_TABS);
+		tabPane.getStyleClass().add(JMetroStyleClass.BACKGROUND);							
 	}
 
 	public void showPage(Page page) {
