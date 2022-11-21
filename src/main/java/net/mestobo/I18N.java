90f246325d1d7630462d03613a9299b0f34b2c43
Patrick Peer <ppe@synedra.com>
2022-11-21 16:22:56 +0100

    Fixed warning by allowing Objects as arguments for I18N.get()

--- src/main/java/net/mestobo/I18N.java
+++ src/main/java/net/mestobo/I18N.java
@@ -13,7 +13,7 @@ public class I18N {
 	public static final String RESOURCE_BUNDLE_NAME = "net.mestobo.mestobo";
 	public static final Locale FALLBACK_LOCALE = Locale.ENGLISH;
 	
-	public static String get(String key, String ... arguments) {
+	public static String get(String key, Object... arguments) {
 		ResourceBundle bundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_NAME);
 		try {
 			return MessageFormat.format(bundle.getString(key), arguments);
