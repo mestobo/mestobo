0ef1243515bb52e94a8c1b7b3ad4f79fdf6388e2
Robert Lichtenberger <r.lichtenberger@gmail.com>
2023-01-31 22:00:34 +0100

    Improve column width for TasksPage.

--- src/main/java/net/mestobo/TasksPage.java
+++ src/main/java/net/mestobo/TasksPage.java
@@ -25,14 +25,17 @@ public class TasksPage extends MenuPage {
 		
 		TableColumn<Task<?>, String> titleColumn = new TableColumn<>(I18N.get("Title"));
 		titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+		titleColumn.prefWidthProperty().bind(table.widthProperty().multiply(0.33));
 		table.getColumns().add(titleColumn);
 		
 		TableColumn<Task<?>, String> stateColumn = new TableColumn<>(I18N.get("Status"));
 		stateColumn.setCellValueFactory(new PropertyValueFactory<>("state"));
+		stateColumn.prefWidthProperty().bind(table.widthProperty().multiply(0.1));
 		table.getColumns().add(stateColumn);
 		
 		TableColumn<Task<?>, String> messageColumn = new TableColumn<>(I18N.get("Message"));
 		messageColumn.setCellValueFactory(new PropertyValueFactory<>("message"));
+		messageColumn.prefWidthProperty().bind(table.widthProperty().multiply(0.55));
 		table.getColumns().add(messageColumn);
 
 		return table;
