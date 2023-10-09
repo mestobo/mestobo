b8c1c719b2757c0c9e3bf77fa7ebf9815ab71c9e
Robert Lichtenberger <r.lichtenberger@synedra.com>
2023-10-09 16:53:51 +0200

    Add Freemarker and dcm4che as libraries.

--- src/main/java/net/mestobo/DicomGeneratorPage.java
+++ src/main/java/net/mestobo/DicomGeneratorPage.java
@@ -1,12 +1,27 @@
 package net.mestobo;
 
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+import com.google.inject.Inject;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import net.mestobo.form.Form;
 import net.mestobo.form.TextFormField;
 
 /** DicomGeneratorPage allows to generate fictitious DICOM studies. */
 public class DicomGeneratorPage extends MenuPage {
+	
+	@Inject
+	private BackgroundTaskExecutor backgroundTaskExecutor;
 	
 	public DicomGeneratorPage() {
 		super(I18N.get("GenerateDicomStudy"));
@@ -31,6 +46,44 @@ public class DicomGeneratorPage extends MenuPage {
 	protected Node createPresentation() {
 		Form form = new Form();
 		form.addField("destination_directory", new TextFormField(I18N.get("DestinationDirectory"))).required();
+		form.addButton(I18N.get("Generate"), "generate", this::generateDicom).withIcon("dashicons-controls-play");
+		
 		return form;
+	}
+	
+	private void generateDicom(ActionEvent e) {
+		File templateFile = new File("/home/rli/test.ftl"); // todo :: get template from template library, etc.
+		backgroundTaskExecutor.submitTask(new GenerateDicomTask(templateFile));
+	}
+	
+	private class GenerateDicomTask extends Task<Void> {
+		
+		File templateFile;
+
+		public GenerateDicomTask(File templateFile) {
+			this.templateFile = templateFile;
+			updateTitle(I18N.get("GenerateDicomStudy"));
+		}
+
+		@Override
+		protected Void call() throws Exception {
+			Configuration cfg = new Configuration(Configuration.VERSION_2_3_32);
+			cfg.setDirectoryForTemplateLoading(templateFile.getParentFile());
+			cfg.setLocalizedLookup(false);
+			Template template = cfg.getTemplate(templateFile.getName());
+			
+			Map<String, Object> data = Map.of("foo", "fighter", "bar", "tender");
+			try(Writer out = new StringWriter()) {
+				template.process(data, out);
+				System.out.println(out.toString());
+			} catch (Exception e) { // ToDo this is duplicated from SendADTTask; How to DRY ?
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				e.printStackTrace(pw);
+				updateMessage(sw.toString());
+				throw e;
+			}
+			return null;
+		}		
 	}
 }
